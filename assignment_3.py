b69ff8124d9378751370c9f337a7cabd35ceccd7
Dragonblade1243 <hustonjacob30@gmail.com>
2025-02-10 00:57:03 -0500

    week 3

---
+++ assignment_3.py
@@ -0,0 +1,26 @@
+
+# Objective: Write a program that takes a list of numbers and prints out the sum, average, and largest number in the list.
+a=4
+b=6
+c=8
+d=10
+print("sum of numbers is:")
+print(sum([a,b,c,d]))
+
+print("average of numbers is:")
+avg= sum([a,b,c,d])/4
+print(avg)
+
+print("largest number is:")
+print(max([a,b,c,d]))
+
+# Problem 1: Write a program that takes a list of numbers and prints out the sum of all the numbers in the list.
+
+
+
+# Problem 2: Write a program that takes a list of numbers and prints out the average of all the numbers in the list.
+
+
+
+# Problem 3: Write a program that takes a list of numbers and prints out the largest number in the list.
+
